6688d0c7091b811d5e5c40141a172cf91e7afe9a
tpichurina <krushinskayatanya@gmail.com>
2021-08-02 22:46:43 +0300

    HW-4 Create Contact tests

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+
+        public ContactData(string firstname, string lastname)
+        {
+           
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        public string Firstname
+
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+        public string Nickname
+
+        {
+            get
+            {
+                return nickname;
+            }
+            set
+            {
+                nickname = value;
+            }
+        }
+        public string Title
+
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public string Company
+
+        {
+            get
+            {
+                return company;
+            }
+            set
+            {
+                company = value;
+            }
+        }
+    }
+}
